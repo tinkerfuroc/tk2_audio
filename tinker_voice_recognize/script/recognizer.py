7d143e44b922a0e4363fe2fd895b3715237ef306
gjc13 <amtcmp@126.com>
2016-06-03 22:41:12 +0800

    [Add] Test client for kaldi

--- tinker_voice_recognize/script/recognizer.py
+++ tinker_voice_recognize/script/recognizer.py
@@ -1,58 +1,94 @@
 #!/usr/bin/python
 
-import rospkg
-import rospy
+import argparse
+from ws4py.client.threadedclient import WebSocketClient
+import time
+import threading
+import sys
+import urllib
+import Queue
+import json
+import time
+import os
+import sys
 import pyaudio
 
-from threading import Thread
-from pocketsphinx.pocketsphinx import *
-from std_msgs.msg import String
-from termcolor import colored
+def rate_limited(maxPerSecond):
+    minInterval = 1.0 / float(maxPerSecond)
+    def decorate(func):
+        lastTimeCalled = [0.0]
+        def rate_limited_function(*args,**kargs):
+            elapsed = time.clock() - lastTimeCalled[0]
+            leftToWait = minInterval - elapsed
+            if leftToWait>0:
+                time.sleep(leftToWait)
+            ret = func(*args,**kargs)
+            lastTimeCalled[0] = time.clock()
+            return ret
+        return rate_limited_function
+    return decorate
 
 
-def recognize():
-    config = Decoder.default_config()
-    config.set_string('-hmm', rospy.get_param('~hmm'))
-    config.set_string('-lm', rospy.get_param('~lm'))
-    config.set_string('-dict', rospy.get_param('~dict'))
-    config.set_string('-fsg', rospy.get_param('~fsg'))
-    config.set_string('-logfn', '/dev/null')
+class KaldiClient(WebSocketClient):
+    def __init__(self, url, protocols=None, extensions=None, heartbeat_freq=None, rate=16000):
+        super(KaldiClient, self).__init__(url, protocols, extensions, heartbeat_freq)
+        self.audio = pyaudio.PyAudio()
+        self.audio_stream = self.audio.open(format=pyaudio.paInt16, 
+                channels=1, rate=rate, 
+                input=True, frames_per_buffer=rate / 2)
+        self.audio_stream.start_stream()
 
-    publisher = rospy.Publisher('/recognizer/output', String, queue_size=10)
 
-    p = pyaudio.PyAudio()
-    stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=1024)
-    stream.start_stream()
+    @rate_limited(4)
+    def send_data(self, data):
+        self.send(data, binary=True)
 
-    decoder = Decoder(config)
-    decoder.start_utt()
-    in_speech_bf = False
-    while not rospy.is_shutdown():
-        buf = stream.read(1024)
-        if buf:
-            decoder.process_raw(buf, False, False)
-            if decoder.get_in_speech() != in_speech_bf:
-                in_speech_bf = decoder.get_in_speech()
-                if not in_speech_bf:
-                    decoder.end_utt()
-                    result = decoder.hyp().hypstr
-                    publisher.publish(result)
-                    rospy.loginfo(colored('Result:', 'green') + '%s', result)
-                    decoder.start_utt()
+    def opened(self):
+        print "Socket opened!"
+        def send_data_to_ws():
+            while True:
+                block = self.audio_stream.read(8000)
+                self.send_data(block)
+            self.send("EOS")
+
+        t = threading.Thread(target=send_data_to_ws)
+        t.start()
+        t.join()
+
+
+    def received_message(self, m):
+        response = json.loads(str(m))
+        #print >> sys.stderr, "RESPONSE:", response
+        #print >> sys.stderr, "JSON was:", m
+        if response['status'] == 0:
+            if 'result' in response:
+                trans = response['result']['hypotheses'][0]['transcript']
+                print trans
+                if response['result']['final']:
+                    print trans
+                    #print >> sys.stderr, trans,
+                    print >> sys.stderr, '\r%s' % trans.replace("\n", "\\n")
         else:
-            break
-    decoder.end_utt()
-    stream.stop_stream()
+            print >> sys.stderr, "Received error from server (status %d)" % response['status']
+            if 'message' in response:
+                print >> sys.stderr, "Error message:",  response['message']
+
+    def closed(self, code, reason=None):
+        pass
 
 
 def main():
-    rospy.init_node('voice_recognize', anonymous=False)
-    rospy.loginfo(colored('starting voice recognize ...', 'green'))
-    thread = Thread(target=recognize)
-    thread.start()
-    rospy.spin()
-    thread.join()
 
+    uri = 'ws://localhost:8080/client/ws/speech?content-type='
+    sample_rate = 16000
+    ws = KaldiClient(uri, rate=sample_rate)
+    ws.connect()
+    while(True):
+        pass
+    #result = ws.get_full_hyp()
+    #print result.encode('utf-8')
+        
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
+
