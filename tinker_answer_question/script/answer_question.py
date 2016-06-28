5a5ef20191970528ffec36f02dad52e7e8f2bafe
gjc13 <amtcmp@126.com>
2016-06-28 14:31:22 +0200

    [Add] hash keywords

--- tinker_answer_question/script/answer_question.py
+++ tinker_answer_question/script/answer_question.py
@@ -8,27 +8,44 @@ from nltk.metrics import edit_distance
 from std_msgs.msg import String
 from termcolor import colored
 
+def build_word_keys(questions, prohibited_words):
+    word_keys = {}
+    key = 0
+    for question in questions.keys():
+        for word in question.split():
+            if word not in word_keys and word not in prohibited_words:
+                word_keys[word] = keys
+                keys += 1
+    return word_keys
+
+
+def to_key_list(sentence, word_keys):
+    return [word_keys[w] for w in sentence.split() if w in word_keys]
+
 
 class Answerer:
     def __init__(self, xml):
         self.question_dict = self.phrase_question(xml)
+        self.prohibited_words = {'where', 'are', 'what', 'of',
+                'or', 'is', 'in', 'which', 'who', 'the'}
+        self.word_keys = build_word_keys(questions, self.prohibited_words)
 
     @staticmethod
     def phrase_question(xml):
         xml_root = ET.parse(xml).getroot()
         questions = {}
         for question in xml_root.findall('question'):
-            questions[question.find('q').text.upper()] = question.find('a').text
+            questions[question.find('q').text.lower()] = question.find('a').text
         return questions
 
     def answer_question(self, msg_question):
-        msg_question = msg_question.data
+        msg_question = msg_question.data.lower()
         if len(msg_question) == 0:
             return
         rospy.loginfo(colored('[Q]%s', 'yellow'), msg_question)
         distance = {}
         for question in self.question_dict:
-            distance[question] = edit_distance(question, msg_question)
+            distance[question] = edit_distance(to_key_list(question), to_key_list(msg_question))
         question = min(distance, key=distance.get)
         if distance[question] > 10:
             return
